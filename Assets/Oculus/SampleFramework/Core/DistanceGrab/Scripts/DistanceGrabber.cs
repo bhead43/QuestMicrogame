8301cbd61895da6c3c6a493d71c6fa9e1888fcb5
Matthew Umana <mumana98@gmail.com>
2020-02-21 21:58:57 -0600

    Small change to Game Win Text in script DistanceGrabber.cs

--- Assets/Oculus/SampleFramework/Core/DistanceGrab/Scripts/DistanceGrabber.cs
+++ Assets/Oculus/SampleFramework/Core/DistanceGrab/Scripts/DistanceGrabber.cs
@@ -156,7 +156,7 @@ namespace OculusSampleFramework
 
             if (evidenceRemaining <= 0)
             {
-                evidenceRemaining = 0; //placeholder
+                EvidenceText.GetComponent<Text>().text = "ALL EVIDENCE FOUND!";
             }
         }
 
